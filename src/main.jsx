481b5015da27f2ea0a719ce79135d3c11d35a7fe
Pyxina <lina.lamhaddan@gmail.com>
2026-02-09 19:50:49 +0100

    mon code

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,14 @@ import './index.css'
 import App from './App.jsx'
 import { BrowserRouter, Routes, Route } from "react-router";
 import PokemonDetails from './screens/pokemonDetails.jsx';
+import PokemonAdd from './screens/pokemonAdd.jsx';
 
 createRoot(document.getElementById('root')).render(
  <BrowserRouter>
     <Routes>
         <Route path="/" element={<App />} />
-        <Route path="/pokemonDetails/:url" element={<PokemonDetails />} />
+        <Route path="/pokemonDetails/:id" element={<PokemonDetails />} />
+        <Route path="/pokemonAdd" element={<PokemonAdd />} />
     </Routes>
 </BrowserRouter>
 ,
